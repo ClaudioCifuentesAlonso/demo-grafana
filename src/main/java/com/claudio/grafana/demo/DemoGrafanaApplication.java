00d01801ea603058b3ef7992898dc30a928738dd
Claudio Cifuentes <claudio_cifuentes@epam.com>
2022-03-08 11:10:02 -0600

    Initial commit

---
+++ src/main/java/com/claudio/grafana/demo/DemoGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.claudio.grafana.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claudio/grafana/demo/DemoGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudio.grafana.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
